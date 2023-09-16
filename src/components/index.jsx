ba74415e3d2e1b3bc1983079cc55e1a59ef9101b
rajkumarrraja <110884663+rajkumarrraja@users.noreply.github.com>
2023-09-16 20:07:30 +0530

    change export

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import Product from './Product';
-import Review from './review';
+import Review from './Review';
 import RelatedProduct from './RelatedProduct';
 
 export { Navbar, Home, Product, Review, RelatedProduct };
